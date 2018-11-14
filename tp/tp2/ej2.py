704bf154d337e4942f6992b700cb70cf7a208911
mscoccimarro <maximiliano.scoccimarro@gmail.com>
2018-11-13 23:07:33 -0300

    Agrego cantidad de solicitudes en cola por tiempo. Agrego cant de veces que el sistema estuvo en cada estado.

--- tp/tp2/ej2.py
+++ tp/tp2/ej2.py
@@ -8,23 +8,38 @@
 #     [ p31 p32 p33 ]   [ 1 0 0 ]
 import numpy as np
 
-matrix = np.matrix([[113/120, 1/40, 1/30], [1, 0, 0], [1, 0, 0]])
+p = np.matrix([[113/120, 1/40, 1/30], [1, 0, 0], [1, 0, 0]])
 stateChangeQ = round((1000 * 1000) / 10)
-# stateCount = [Esperando, Arribo Solicitud, Termina Proceso Solicitud]
-stateCount = [0, 0, 0]
-
-print(matrix)
-print(stateChangeQ)
 
 # Empieza esperando
-stateCount[0] += 1
-currentState = np.matrix([[1, 0, 0]])
-
-for i in range(0, 3):
+currentState = [1, 0, 0]
+stateCount = [0, 0, 0]
+solCount = 0
+stateNum = 1
+for i in range(0, stateChangeQ):
+  print('En estado ', stateNum, ':')
+  stateCount[stateNum-1] += 1
+  print(solCount, ' solicitudes en cola a los ', i*10, ' milisegundos')
+  d = np.matmul(currentState, p**(i+1))
+  probValues = d.tolist()[0]
   prob = np.random.uniform(0, 1)
-  if (prob <= 113/120):
-    currentState = np.matrix([[1, 0, 0]])
-  else if (prob <= 29/30):
-    currentState = np.matrix([[0, 1, 0]])
+  # print(probValues)
+  if (prob <= probValues[0]):
+    stateNum = 1
+    currentState = [1, 0, 0]
+  elif (prob <= (probValues[0] + probValues[1])):
+    stateNum = 2
+    currentState = [0, 1, 0]
+    solCount += 1
   else:
-    currentState = np.matrix([[0, 0, 1]])
+    stateNum = 3
+    currentState = [0, 0, 1]
+    if (solCount > 0):
+      solCount -= 1
+      print('Solicitud procesada')
+  print('')
+
+print('En estado ', stateNum)
+stateCount[stateNum-1] += 1
+print(solCount, ' solicitudes en cola a los ', i*10, ' milisegundos')
+print('Cantidad de veces en cada estado', stateCount)
